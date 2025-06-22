06a3ec06527b49ba359bf8dd3c3994146b7722cf
Thalys Barbosa <thalysvinicius23@gmail.com>
2025-06-22 19:12:58 -0300

    Add additional test cases for task editing and completion

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -68,4 +68,38 @@ describe('TODOMvc App', () => {
       .children()
       .should('have.length', 2);
   });
+
+  it('Edita uma tarefa existente', () => {
+    cy.visit('');
+    cy.get('[data-cy=todo-input]')
+      .type('Estudar Cypress{enter}');
+    cy.get('[data-cy=todos-list] > li')
+      .dblclick();
+    cy.get('[data-cy=edit-todo-input]')
+      .clear()
+      .type('Estudar Cypress Avançado{enter}');
+    cy.get('[data-cy=todos-list] > li')
+      .should('have.text', 'Estudar Cypress Avançado');
+  });
+
+  it('Marca e desmarca uma tarefa como concluída', () => {
+    cy.visit('');
+    cy.get('[data-cy=todo-input]')
+      .type('Ler documentação{enter}');
+    cy.get('[data-cy=toggle-todo-checkbox]')
+      .click()
+      .should('be.checked');
+    cy.get('[data-cy=toggle-todo-checkbox]')
+      .click()
+      .should('not.be.checked');
+  });
+
+  it('Não adiciona tarefa vazia', () => {
+    cy.visit('');
+    cy.get('[data-cy=todo-input]')
+      .type('{enter}');
+    cy.get('[data-cy=todos-list]')
+      .children()
+      .should('have.length', 0);
+  });
 });
